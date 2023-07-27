f1d3acfd1e555181d69ed2a428fb1d1a80fd8d7c
Will Parker <will.parker@smokeball.com>
2023-07-27 18:16:44 +1000

    Updated scryptsy to work with React Native & Expo

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+0.0.1 / 2023-07-27
+------------------
+- initial release. Forked from [https://github.com/cryptocoinjs/scryptsy](https://github.com/cryptocoinjs/scryptsy) and switched NodeJS crypto lib for [react-native-quick-crypto](https://github.com/margelo/react-native-quick-crypto).
---
+++ test/scrypt.js
@@ -0,0 +1,27 @@
+/* global describe, it */
+
+const assert = require('assert')
+const scrypt = require('../')
+
+const fixtures = require('./fixtures')
+
+// some tests params from: https://github.com/barrysteyn/node-scrypt/blob/master/tests/scrypt-tests.js
+// also: https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00
+// note, that params are wrong on here https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00 see:
+// => https://twitter.com/dchest/status/247734446881640448
+
+describe('scrypt', function () {
+  describe('> when valid', function () {
+    fixtures.valid.forEach(function (f) {
+      if (f.skip) return // impractical to run most times
+
+      it('should compute for ' + f.description, async function () {
+        var data1 = await scrypt.async(f.key, f.salt, f.iterations, f.memory, f.parallel, f.keyLen)
+        var data2 = scrypt(f.key, f.salt, f.iterations, f.memory, f.parallel, f.keyLen)
+
+        assert.strictEqual(data1.toString('hex'), f.result)
+        assert.strictEqual(data2.toString('hex'), f.result)
+      })
+    })
+  })
+})
